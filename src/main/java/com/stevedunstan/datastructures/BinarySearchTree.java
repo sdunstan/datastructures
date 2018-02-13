dab7fb108f7ab398e21853d36006663a475d14fb
Steve Dunstan <steve_dunstan@yahoo.com>
2018-02-12 21:10:06 -0700

    basic binary tree working. balancing is broken

--- src/main/java/com/stevedunstan/datastructures/BinarySearchTree.java
+++ src/main/java/com/stevedunstan/datastructures/BinarySearchTree.java
@@ -7,30 +7,70 @@ public class BinarySearchTree<Key extends Comparable<Key>,Value> {
 
     private Node root;
 
-    // Rotate the node on my right to me (to the left).
-    private Node rotateLeft(final Node node) {
-        Node newLeftNode = new Node(
-                node.getKey(),
-                node.getValue(),
-                node.getSubtreeSize(),
-                node.getColor(),
-                node.getLeft(),
-                node.getRight().getLeft());
-
-        
-        Node newNode = new Node(
-                node.getRight().getKey(),
-                node.getRight().getValue(),
-                node.getSubtreeSize(),
-                node.getColor(),
-                newLeftNode,
-                node.getRight().getRight());
-
-        return newNode;
+    private int size(Node node) {
+        if (node == null) return 0;
+        return node.getSubtreeSize();
     }
 
-    private Node rotateRight(Node node) {
-        return null;
+    // Rotate the node on my right to the left. Return the left rotated node.
+    //     B
+    //    / \
+    //   A   D
+    //      / \
+    //     C   E
+    // Becomes:
+    //       D
+    //      / \
+    //     B   E
+    //    / \
+    //   A   C
+    private Node rotateLeft(final Node node) {
+        Node nodeRight = node.getRight();
+        Color nodeColor = node.getColor();
+        int nodeSubtreeSize = node.getSubtreeSize();
+
+        Node nodeLeft = new Node(
+                node.getKey(),
+                node.getValue(),
+                1 + size(node.getLeft()) + ((nodeRight == null) ? 0 : size(nodeRight.getLeft())),
+                RED,
+                node.getLeft(),
+                ((nodeRight == null) ? null : nodeRight.getLeft()));
+
+        nodeRight = new Node(
+                nodeRight.getKey(),
+                nodeRight.getValue(),
+                nodeSubtreeSize,
+                nodeColor,
+                nodeLeft,
+                nodeRight.getRight());
+
+        return nodeRight;
+    }
+
+    // Make money-money. Make money-money. Wikki, wikki, wikki WHOOO!
+    private Node rotateRight(final Node node) {
+        Node nodeLeft = node.getLeft();
+        Color nodeColor = node.getColor();
+        int nodeSubtreeSize = node.getSubtreeSize();
+
+        Node nodeRight = new Node(
+                node.getKey(),
+                node.getValue(),
+                1 + size(node.getLeft()) + size(nodeLeft.getRight()),
+                RED,
+                nodeLeft.getRight(),
+                node.getRight());
+
+        nodeLeft = new Node(
+                nodeLeft.getKey(),
+                nodeLeft.getValue(),
+                nodeSubtreeSize,
+                nodeColor,
+                nodeLeft.getLeft(),
+                nodeRight);
+
+        return nodeLeft;
     }
 
     /**
@@ -40,14 +80,14 @@ public class BinarySearchTree<Key extends Comparable<Key>,Value> {
         Node left = new Node(
                 node.getLeft().getKey(),
                 node.getLeft().getValue(),
-                node.getLeft().getSubtreeSize(),
+                size(node.getLeft()),
                 BLACK,
                 node.getLeft().getLeft(),
                 node.getLeft().getRight());
         Node right = new Node(
                 node.getRight().getKey(),
                 node.getRight().getValue(),
-                node.getRight().getSubtreeSize(),
+                size(node.getRight()),
                 BLACK,
                 node.getRight().getLeft(),
                 node.getRight().getRight());
@@ -81,24 +121,32 @@ public class BinarySearchTree<Key extends Comparable<Key>,Value> {
         else newNode = copyWithValue(node, value);
 
         // Now rotate, if necessary
-        if (isRed(newNode.getLeft()) && isBlack(newNode.getRight()))
-            newNode = rotateLeft(newNode);
-        if (isRed(newNode.getLeft()) && isRed(newNode.getLeft().getLeft()))
-            newNode = rotateRight(newNode);
+        //if (isRed(newNode.getLeft()) && isBlack(newNode.getRight()))
+        //    newNode = rotateLeft(newNode);
+        //if (isRed(newNode.getLeft()) && isRed(newNode.getLeft().getLeft()))
+        //    newNode = rotateRight(newNode);
 
         // Now flip colors, if necessary
-        if (isRed(newNode.getLeft()) && isRed(newNode.getRight()))
-            newNode = flipColors(newNode);
+        //if (isRed(newNode.getLeft()) && isRed(newNode.getRight()))
+        //    newNode = flipColors(newNode);
 
         newNode = incrementSize(newNode);
 
         return newNode;
     }
 
+    private Value get(Node node, Key key) {
+        if (node == null) return null;
+        int diff = key.compareTo(node.getKey());
+        if      (diff < 0) return get(node.left, key);
+        else if (diff > 0) return get(node.right, key);
+        else               return node.getValue();
+    }
+
     private Node incrementSize(Node node) {
         return new Node(node.getKey(),
                 node.getValue(),
-                node.getLeft().getSubtreeSize() + node.getRight().getSubtreeSize() + 1,
+                size(node.getLeft()) + size(node.getRight()) + 1,
                 node.getColor(),
                 node.getLeft(),
                 node.getRight());
@@ -142,10 +190,10 @@ public class BinarySearchTree<Key extends Comparable<Key>,Value> {
     }
 
     public Value get(Key key) {
-        return root.getValue();
+        return get(root, key);
     }
 
-    final class Node {
+    private final class Node {
         private final Key key;
         private final Value value;
         private final Node left, right;
@@ -183,10 +231,6 @@ public class BinarySearchTree<Key extends Comparable<Key>,Value> {
 
         public boolean isRed() {
             return color == RED;
-        }
-
-        public boolean isBlack() {
-            return color == BLACK;
         }
 
         public Color getColor() {
--- src/test/java/com/stevedunstan/datastructures/BinarySearchTreeTest.java
+++ src/test/java/com/stevedunstan/datastructures/BinarySearchTreeTest.java
@@ -15,4 +15,25 @@ class BinarySearchTreeTest {
         assertEquals("value", bst.get("key"));
     }
 
+    @Test
+    void putAndGetStrings() {
+        BinarySearchTree bst = new BinarySearchTree<String,String>();
+        bst.put("B", "0");
+        bst.put("S", "1");
+        bst.put("E", "2");
+        bst.put("A", "3");
+        bst.put("R", "4");
+        bst.put("C", "5");
+        bst.put("H", "6");
+
+        assertEquals(7, bst.size());
+        assertEquals("0", bst.get("B"));
+        assertEquals("1", bst.get("S"));
+        assertEquals("2", bst.get("E"));
+        assertEquals("3", bst.get("A"));
+        assertEquals("4", bst.get("R"));
+        assertEquals("5", bst.get("C"));
+        assertEquals("6", bst.get("H"));
+    }
+
 }
